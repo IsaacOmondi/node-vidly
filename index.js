179983ff909790c0c350eb445cf1b182b726d64c
Isaac Omondi <iochieng@sendy.co.ke>
2019-04-05 15:00:28 +0300

    creating and implementing custom middleware

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ require('dotenv').config();
 const express = require('express');
 const movies = require('./routes/movies');
 const home = require('./routes/home');
+const authentication = require('./logger');
 
 const app = express();
+
+app.use((req, res, next) => {
+    console.log('Logging...');
+    next();
+});
+app.use(authentication);
 
 app.use('/', home);
 app.use('/api/movies/', movies);
 app.use(express.json());
+app.use(express.static('public'));
 
 
 const port = process.env.PORT;
